8f8b723c7e5788a772147b2048c3d5f8c15ecd5a
Esteban Blanco <eblanco707@gmail.com>
2016-09-23 16:42:14 -0600

    Controlador estadisticas y una pichita en conexion

--- appWEB/estadisticas/controlador/estadisticas.js
+++ appWEB/estadisticas/controlador/estadisticas.js
@@ -1,7 +1,94 @@
 angular.module('AdminSqlServer')
-.controller("estadisticasCtrl", function($scope,$http,$location)
+.controller("ControladorEstadisticas", function($scope,$http,serveData)
 {
+    console.log("Controlador Estadisticas");
+    $scope.VerGrafico;
+    $scope.nombreArchivo;
+    $scope.tOcupado;
+    $scope.tDisponible;
+    $scope.tActual;
+    $scope.tMax;
+    var ctx;
     
+    
+    $scope.ObtenerArchivos = function()
+    {
+        $scope.url = "http://localhost:8080/ProyectoBD/ConexionBD/conexion.php?Funcion=ObtenerArchivosBD";
+        console.log(serveData.dbName, serveData.userName, serveData.password)
+        $scope.data = {"dbName":serveData.dbName,"userName":serveData.userName,"password":serveData.password}; 
+        $http.post($scope.url,$scope.data).success( function(response){
+            if(response){ 
+                console.log(response);
+                $scope.listaArchivos = response;
+            }
+            else{
+                alert("..¡Fallo la conecion con Sql Server.");
+            }
+        }); 
+    };
+    
+    $scope.ObtenerArchivos();
+    $scope.ObtenerEstadisticasArchivo = function()
+    {
+        $scope.url = "http://localhost:8080/ProyectoBD/ConexionBD/conexion.php?Funcion=ObtenerInformacionEstadistica";
+        $scope.data = {"dbName":serveData.dbName,"userName":serveData.userName,"password":serveData.password,"nombreArchivo":$scope.nombreArchivo};
+        $http.post($scope.url,$scope.data).success( function(response){
+            if(response){ 
+                console.log(response);
+                $scope.tMax = response[0].TamanoMaximo;
+                $scope.tOcupado = response[0].EspacioUtilizado;
+                $scope.tDisponible = response[0].EspacioLibre;
+                $scope.tActual = response[0].EspacioTotal;
+                
+                console.log($scope.tOcupado,$scope.tDisponible,$scope.tActual,$scope.tMax)
+                $scope.VerGrafico();
+            }
+            else{
+                alert("..¡Fallo la conecion con Sql Server.");
+            }
+        }); 
+    };
+   
+  
+    
+    
+    $scope.VerGrafico = function()
+    {
+        ctx= document.getElementById("myChart");
+
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Ocupado", "Disponible", "Tamaño", "Tamaño Máximo"],
+                datasets: [{
+                    label: ['Espacio Ocupado','Espacio Disponible','Tamaño Archivo','Tamaño Máximo'],
+                    data: [($scope.tOcupado/$scope.tMax)*100, ($scope.tDisponible/$scope.tMax)*100, ($scope.tActual/$scope.tMax)*100, 100],
+                    backgroundColor: [
+                        'rgba(248, 6, 6, 0.9)',
+                        'rgba(22, 247, 176, 0.9)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)'
+
+                    ],
+                    borderColor: [
+                        'rgb(225, 9, 9, 1)',
+                        'rgb(4, 199, 209, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+  
 });
-
-
